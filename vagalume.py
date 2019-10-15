cca80b42312b0cc30a58ae76918b7ff974e94289
lucassoccol <luks.14@hotmail.com>
2019-10-14 22:57:08 -0300

    add hamming

--- vagalume.py
+++ vagalume.py
@@ -7,16 +7,16 @@ cidades = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 
 melhor_caminho = {'distancia': 100000, 'caminho': []}
 
 distancias = """
-0   4   5   6   11  13  20  2   60  16  
-4   0   3   6   7   11  34  23  6   90
-5   3   0   3   8   8   40  17  22  21
-6   6   3   0   11  9   33  2   1   15
-11  7   8   11  0   4   88  77  93  11
-13  11  8   9   4   0   12  14  22  1
-20  34  40  33  88  12  0   81  20  17
-2   23  17  2   77  14  81  0   27  54
-60  6   22  1   93  22  20  27  0   99
-16  90  21  15  11  1   17  54  99  0
+0   4   5   6   11  13  20  2   60  16  20
+4   0   3   6   7   11  34  23  6   90  10 
+5   3   0   3   8   8   40  17  22  21  5
+6   6   3   0   11  9   33  2   1   15  74
+11  7   8   11  0   4   88  77  93  11  14 
+13  11  8   9   4   0   12  14  22  1   36
+20  34  40  33  88  12  0   81  20  17  20
+2   23  17  2   77  14  81  0   27  54  10
+60  6   22  1   93  22  20  27  0   99  55 
+16  90  21  15  11  1   17  54  99  0   11
 """
 
 aux = [x.split() for x in distancias.splitlines()[1:]]
@@ -24,7 +24,7 @@ matriz = [[int(j) for j in vetor] for vetor in aux]
 
 n_vagalumes = 5
 vagalumes = []
-absorcao = 0.1
+absorcao = 1.0
 
 v = ['b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
@@ -73,36 +73,45 @@ def change (vetor, posa, posb):
     vetor[posb] = aux
 
 #faz os swap para igualar os vetores
-def _swap(vetora, vetorb):
+def _swap(vetora, vetorb, num_change):
+    sc = 0 
+    for i in range(1, len(vetorb)-1):
+        if not vetorb[i] == vetora[i]:
+            sc += 1         
+            change(vetora, i, vetora.index(vetorb[i]))
+            if sc == num_change:
+                break
+
+def _swap_count(vetora, vetorb):
     sc = 0 
     for i in range(1, len(vetorb)-1):
         if not vetorb[i] == vetora[i]:
             sc += 1
-            change(vetora, i, vetora.index(vetorb[i]))
     return sc
 
 #comparar todos com todos e if (xi < xj) xi movimenta-se para xj // calcula distancia xj - xi
 def compara_vagalumes (vetor):
     for i in range(len(vetor)):
         for j in range(len(vetor)):
-            diferenca = abs(retorna_custo(vetor[j]) - retorna_custo(vetor[i]))
-            atratividade = absorcao * diferenca
+            #diferenca = abs(retorna_custo(vetor[j]) - retorna_custo(vetor[i]))
+            #atratividade = absorcao * diferenca
+            #atratividade = absorcao * _swap_count(vetor[i], vetor[j])
+            aleatorio = vetor[i].copy()
             if retorna_custo(vetor[i]) > retorna_custo(vetor[j]):
-                #mova i para j 
-                aleatorio = gen_vag_otm(1, retorna_custo(vetor[i]), retorna_custo(vetor[i])-atratividade)            
-                if aleatorio:
-                    vetor[i] = aleatorio[0]
+                #gera aleatorio e mova i para j   
+                # colocar fator de atratividade aqui 
+                _swap(aleatorio, vetor[j], (random.randrange(1, _swap_count(vetor[i], vetor[j]), 1)))       
+                if retorna_custo(aleatorio) <= retorna_custo(vetor[i]):
+                    vetor[i] = aleatorio
             else:
-                aleatorio = gen_vag_otm(1, retorna_custo(vetor[i]), retorna_custo(vetor[i])-atratividade)
-                if aleatorio:
-                    vetor[i] = aleatorio[0]
-
-
+                change(aleatorio, random.randrange(1, len(v)-1, 1), random.randrange(1, len(v)-1, 1))
+                if retorna_custo(vetor[i]) > retorna_custo(aleatorio):
+                    vetor[i] = aleatorio
 
 
 def main():
     vagalumes = gera_vagalumes(n_vagalumes)
-    '''
+    
     for v in vagalumes:
         print(v, "-- custo: ", retorna_custo(v))
     
@@ -110,17 +119,8 @@ def main():
         compara_vagalumes(vagalumes)
         print('-----------------------------', i+1, 'loop -----------------------------------')
         for v in vagalumes:
-            print(v, "-- custo: ", retorna_custo(v))'''
-    
-    print(vagalumes[0])
-    print(vagalumes[1])
+            print(v, "-- custo: ", retorna_custo(v))    
 
-
-    _swap(vagalumes[0], vagalumes[1])
-
-    print(vagalumes[0])
-    print(vagalumes[1])
-        
  
 main()
 
